e62a699032fc8a48d0edc1da8b86427c3ab5f188
Kevawn Warren <kevawnw@gmail.com>
2022-05-10 16:06:25 -0400

    updated

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,5 +1,8 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :movie_id, :rating, :review, :date
+  attributes :id, :user_id, :movie_id, :rating, :review, :date, :userinfo
   belongs_to :movie
-  belongs_to :user
+  # belongs_to :user
+  def userinfo
+    object.user
+  end
 end
